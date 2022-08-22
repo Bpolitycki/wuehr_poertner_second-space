a6160687444347dab64c91de8a0f12fa62a83264
Bpolitycki <42381332+Bpolitycki@users.noreply.github.com>
2022-08-22 12:15:06 +0200

    [feat]: implemented search view

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="loadingwrapper" out:fade>
+        <div class="spinner">
+            <div />
+            <div />
+        </div>
+        <p>Daten werden geladen</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.spinner {
+    width: 100px;
+    height: 100px;
+    position: relative;
+    margin: 0 auto;
+}
+
+.spinner div {
+    box-sizing: border-box;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: 8px solid transparent;
+    border-top-color: $primary;
+    border-radius: 50%;
+    animation: spinnerOne 1.2s linear infinite;
+}
+
+.spinner div:nth-child(2) {
+    border: 8px solid transparent;
+    border-bottom-color: rgb(25, 78, 101);
+    animation: spinnerTwo 1.2s linear infinite;
+}
+
+.loadingwrapper p {
+    text-align: center;
+    margin-top: 1.5rem;
+}
+
+@keyframes spinnerOne {
+    0% {
+        transform: rotate(0deg);
+        border-width: 10px;
+    }
+
+    50% {
+        transform: rotate(180deg);
+        border-width: 1px;
+    }
+
+    100% {
+        transform: rotate(360deg);
+        border-width: 10px;
+    }
+}
+
+@keyframes spinnerTwo {
+    0% {
+        transform: rotate(0deg);
+        border-width: 1px;
+    }
+
+    50% {
+        transform: rotate(180deg);
+        border-width: 10px;
+    }
+
+    100% {
+        transform: rotate(360deg);
+        border-width: 1px;
+    }
+}
+</style>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,3 +1,67 @@
+<script lang="ts" setup>
+import Result from "@/components/Result.vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue"
+import { ref, onMounted, watch } from 'vue';
+import { useDataStore } from "@/stores/data";
+import { storeToRefs } from "pinia";
+
+const store = useDataStore();
+const { filteredData } = storeToRefs(store);
+
+const query = ref("")
+const isLoading = ref(false)
+
+function handleInput() {
+    if (query.value.length >= 2) {
+        store.filterByTitleOrId(query.value)
+        isLoading.value = true
+        setTimeout(() => {
+            isLoading.value = false
+        }, 750);
+    } else {
+        isLoading.value = false
+    }
+
+}
+
+watch(query, () => {
+    if (query.value.length === 0) {
+        isLoading.value === false
+        store.reset()
+    }
+})
+
+
+onMounted(() => {
+    /* do things on mount */
+    store.reset()
+});
+
+</script>
+
+
 <template>
-  <p>The Search Page</p>
+    <section class="section">
+        <div class="container box p-2">
+            <input class="input" type="text" v-model="query" @submit.prevent @keypress="handleInput"
+                placeholder="Nach Titel oder ID suchen">
+        </div>
+        <div class="container mt-6">
+            <article class="message is-warning" v-if="query.length >= 2 && !filteredData">
+                <div class="message-body">
+                    Keine Ergebnissen gefunden für <span class="is-italic">{{ query }}</span>
+                </div>
+            </article>
+            <article class="message is-danger" v-else-if="query.length > 0 && query.length < 2">
+                <div class="message-body">
+                    Mindestlänge für gesuchten Titel oder ID: 2
+                </div>
+            </article>
+            <Result v-else-if="filteredData && !isLoading" :item="entry" :hr="index !== filteredData.length - 1"
+                :context="'search'" :showImg="true" v-for="(entry, index) in filteredData" />
+            <LoadingSpinner :class="'mt-6'" v-if="isLoading" />
+        </div>
+
+    </section>
+
 </template>
